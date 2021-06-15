372de5942808509eb4d5c08d6beaa72c4835e561
sageata <rpaul.sarbu@yahoo.com>
2021-06-15 10:15:02 +0100

    Fixed iOS errors and exposed ServerSideVerificationOptions in the main file.

--- lib/native_admob_flutter.dart
+++ lib/native_admob_flutter.dart
@@ -1,6 +1,12 @@
 export 'src/events.dart';
 export 'src/mobile_ads.dart';
-export 'src/utils.dart' show AdError, LoadShowAd, UniqueKeyMixin, AdBuilder;
+export 'src/utils.dart'
+    show
+        AdError,
+        LoadShowAd,
+        UniqueKeyMixin,
+        AdBuilder,
+        ServerSideVerificationOptions;
 
 export 'src/native/native_ad_widget.dart';
 export 'src/native/controller/controller.dart';
