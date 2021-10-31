62f5eb65cb1bbd2419f36c7760859636f0d359a4
Zahid Usman Cheema <zahidusmancheema@gmail.com>
2021-10-31 14:11:16 +0500

    Fixed the AdMediaView size warning.

--- example/lib/screens/native_ads.dart
+++ example/lib/screens/native_ads.dart
@@ -59,120 +59,8 @@ class _NativeAdsState extends State<NativeAds>
       },
       child: ListView(padding: EdgeInsets.all(10), children: [
         if (controller.isLoaded)
-          NativeAd(
-            controller: controller,
-            height: 60,
-            builder: (context, child) {
-              return Material(
-                elevation: 8,
-                child: child,
-              );
-            },
-            buildLayout: adBannerLayoutBuilder,
-            loading: Text('loading'),
-            error: Text('error'),
-            icon: AdImageView(padding: EdgeInsets.only(left: 6)),
-            headline: AdTextView(style: TextStyle(color: Colors.black)),
-            advertiser: AdTextView(style: TextStyle(color: Colors.black)),
-            body:
-                AdTextView(style: TextStyle(color: Colors.black), maxLines: 1),
-            media: AdMediaView(height: 70, width: 120),
-            button: AdButtonView(
-              margin: EdgeInsets.only(left: 6, right: 6),
-              textStyle: TextStyle(color: Colors.green, fontSize: 14),
-              elevation: 18,
-              elevationColor: Colors.amber,
-            ),
-          ),
-        SizedBox(height: 10),
         NativeAd(
-          height: 115,
-          builder: (context, child) {
-            return Material(
-              elevation: 8,
-              child: child,
-            );
-          },
-          buildLayout: smallAdTemplateLayoutBuilder,
-          // buildLayout: secondBuilder,
-          loading: Text('loading'),
-          error: Text('error'),
-          icon: AdImageView(size: 70),
-          headline: AdTextView(
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-            maxLines: 1,
-          ),
-          body: AdTextView(style: TextStyle(color: Colors.black)),
-          advertiser: AdTextView(style: TextStyle(color: Colors.black)),
-          media: AdMediaView(height: 80, width: 120),
-          button: AdButtonView(
-            decoration: AdDecoration(backgroundColor: Colors.blue),
-            textStyle: TextStyle(color: Colors.white),
-          ),
-          attribution: AdTextView(
-            width: WRAP_CONTENT,
-            text: 'Ad',
-            decoration: AdDecoration(
-              border: BorderSide(color: Colors.green, width: 2),
-              borderRadius: AdBorderRadius.all(16.0),
-            ),
-            style: TextStyle(color: Colors.green),
-            padding: EdgeInsets.symmetric(horizontal: 4.0, vertical: 1.0),
-          ),
-        ),
-        SizedBox(height: 10),
-        NativeAd(
-          height: 320,
-          unitId: MobileAds.nativeAdVideoTestUnitId,
-          builder: (context, child) {
-            return Material(
-              elevation: 8,
-              child: child,
-            );
-          },
-          buildLayout: mediumAdTemplateLayoutBuilder,
-          // buildLayout: fullBuilder,
-          loading: Text('loading'),
-          error: Text('error'),
-          icon: AdImageView(size: 40),
-          headline: AdTextView(
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-            maxLines: 1,
-          ),
-          body: AdTextView(style: TextStyle(color: Colors.black), maxLines: 1),
-          media: AdMediaView(
-            height: 170,
-            width: MATCH_PARENT,
-          ),
-          attribution: AdTextView(
-            width: WRAP_CONTENT,
-            text: 'Ad',
-            decoration: AdDecoration(
-              border: BorderSide(color: Colors.green, width: 2),
-              borderRadius: AdBorderRadius.all(16.0),
-            ),
-            style: TextStyle(color: Colors.green),
-            padding: EdgeInsets.symmetric(horizontal: 4.0, vertical: 1.0),
-          ),
-          button: AdButtonView(
-            elevation: 18,
-            decoration: AdDecoration(backgroundColor: Colors.blue),
-            height: MATCH_PARENT,
-            textStyle: TextStyle(color: Colors.white),
-          ),
-          ratingBar: AdRatingBarView(starsColor: Colors.white),
-        ),
-        SizedBox(height: 10),
-        NativeAd(
-          height: 100,
+          height: 140,
           builder: (context, child) {
             return Material(
               elevation: 8,
@@ -182,7 +70,7 @@ class _NativeAdsState extends State<NativeAds>
           buildLayout: secondBuilder,
           loading: Text('loading'),
           error: Text('error'),
-          icon: AdImageView(size: 80),
+          icon: AdImageView(size: 40),
           headline: AdTextView(
             style: TextStyle(
               fontSize: 16,
@@ -191,7 +79,7 @@ class _NativeAdsState extends State<NativeAds>
             ),
             maxLines: 1,
           ),
-          media: AdMediaView(height: 80, width: 120),
+          media: AdMediaView(height: 120, width: 120),
         ),
         SizedBox(height: 10),
         NativeAd(
@@ -307,7 +195,7 @@ AdLayoutBuilder get secondBuilder => (ratingBar, media, icon, headline,
           ),
         ),
         children: [
-          icon,
+          media,
           AdLinearLayout(
             children: [
               headline,
